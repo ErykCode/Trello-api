ef8053c5ea7630c1a1da9781319f553925f8d568
ErykCode <phamminhduc5753@gmail.com>
2024-06-24 04:23:37 +0700

    done env

--- src/config/mongodb.js
+++ src/config/mongodb.js
@@ -4,15 +4,13 @@
  * "A bit of fragrance clings to the hand that gives flowers!"
  */
 
-const MONGODB_URI = 'mongodb+srv://ErykCode:6TlYnsFh4wvpo8Or@cluster0.yti0um5.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0';
-const DATABASE_NAME = 'trello-web-eryk';
-
 import { MongoClient, ServerApiVersion } from "mongodb";
+import { env } from "./environment";
 
 let trelloDatabaseInstance = null;
 
 // Khởi tạo MongoClient với từ khóa 'new'
-const mongoClientInstance = new MongoClient(MONGODB_URI, {
+const mongoClientInstance = new MongoClient(env.MONGODB_URI, {
   serverApi: {
     version: ServerApiVersion.v1,
     strict: true,
@@ -25,12 +23,17 @@ export const CONNECT_DB = async () => {
   try {
     await mongoClientInstance.connect();
     console.log("Connected successfully to MongoDB");
-    trelloDatabaseInstance = mongoClientInstance.db(DATABASE_NAME);
+    trelloDatabaseInstance = mongoClientInstance.db(env.DATABASE_NAME);
   } catch (error) {
     console.error("Failed to connect to MongoDB:", error);
     throw error; // Đảm bảo lỗi được ném ra để có thể xử lý bên ngoài
   }
 };
+
+// Đóng kết nối tới Database khi cần
+export const CLOSE_DB = async () => {
+  await mongoClientInstance.close()
+}
 
 export const GET_DB = () => {
   if (!trelloDatabaseInstance) throw new Error("Must connect to DB first");
